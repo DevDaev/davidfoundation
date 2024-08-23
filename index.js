ee4feb2f8ad99f464b0f70989d0de5aafdac50be
DevDaev <devdaev01@gmail.com>
2024-08-23 01:54:13 +0100

    edited files and added a js file

---
+++ index.js
@@ -0,0 +1,8 @@
+var btn = document.getElementById('donate')
+var btn2 = document.getElementById('volunteer')
+
+btn.addEventListener('click', load)
+
+function load() {
+  alert("We are working with the best team to integrate the payment section to this website for you \n We are sorry for any incovenience")
+}
